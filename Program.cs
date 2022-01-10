287f324bbacbfbf7673cd0296f110cc1348fdab3
kulesy <kulewidak@gmail.com>
2022-01-10 14:33:18 +1300

    Command line can open files in dedicated folder within solution

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace AppLoader
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter The App You Want To Run:");
+            var appToStart = Console.ReadLine();
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = $"/C start {appToStart}";
+            startInfo.WorkingDirectory = "C:\\Users\\The Little Monkey\\source\\repos\\AppLoader\\Apps";
+            process.StartInfo = startInfo;
+            process.Start();
         }
     }
 }
